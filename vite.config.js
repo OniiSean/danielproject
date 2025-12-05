12dee9d349886f4e8639705ca9ec7d192ec28956
OniiSean <subombay@gmail.com>
2025-12-05 12:10:16 -0800

    Fixing some bugs with the github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    // ⬇️ IMPORTANT: change this to your actual repo name
-    base: "/danielproject/",
 });
